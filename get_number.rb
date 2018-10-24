7a8e93ed2bf7dfb8d0eaaa9033e610d457df25d2
cgibanez <cgibanez@tuenti.com>
2018-10-24 18:47:58 +0200

    second requirement: generate random number to be guessed

--- get_number.rb
+++ get_number.rb
@@ -10,3 +10,8 @@ input = gets
 name = input.chomp
 
 puts "Welcome, #{name}!"
+
+# Store a randmom number for the player to guess.
+puts "I've got a random number between 1 and 100."
+puts "Can you guess it?"
+target = rand(100) + 1
