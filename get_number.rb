7da297d5ef3f79390b94402ecedcfd2556d5937a
cgibanez <cgibanez@tuenti.com>
2018-10-24 18:44:17 +0200

    First requirement

---
+++ get_number.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+
+# Get My Number Game
+# Written by me
+
+puts "Welcome to 'Get My Number!'"
+print "What's your name? "
+
+input = gets
+name = input.chomp
+
+puts "Welcome, #{name}!"
