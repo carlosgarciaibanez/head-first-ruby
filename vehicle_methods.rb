5c8e408c7b59d78ba17c04e51326682b10d48caa
cgibanez <cgibanez@tuenti.com>
2018-10-26 10:10:53 +0200

    Define and use methods

---
+++ vehicle_methods.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+
+def accelerate
+    puts "Steepping on the gas"
+    puts "Speeding up"
+end
+
+def sound_horn
+    puts "Pressing the horn button"
+    puts "Beep beep!"
+end
+
+def use_headlights(brightness)
+    puts "Turning on #{brightness} headlights"
+    puts "Watch out for deers!"
+end
+
+sound_horn
+accelerate
+use_headlights("high-beam")
