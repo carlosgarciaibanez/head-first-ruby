347018f6a56b78d93ef95d1132625c520719192c
cgibanez <cgibanez@tuenti.com>
2018-10-29 18:49:23 +0100

    Adding instance variables

--- animals.rb
+++ animals.rb
@@ -10,11 +10,23 @@ class Bird
 end
 
 class Dog
+    
+    def make_up_name
+        @name = "Sandy"
+    end
+    def make_up_age
+        @age = 5
+    end
+
     def talk
-        puts "Bark!"
+        puts "#{@name} says Bark!"
     end
     def move(destination)
-        puts "Running to the #{destination}"
+        puts "#{@name} runs to the #{destination}"
+    end
+
+    def report_age
+        puts "#{@name} is #{@age} years old"
     end
 end
 
@@ -29,9 +41,12 @@ end
 
 bird = Bird.new
 dog = Dog.new
+dog.make_up_name
+dog.make_up_age
 cat = Cat.new
 
 bird.move("tree")
 dog.talk
+dog.report_age
 bird.talk
 cat.move("house")
