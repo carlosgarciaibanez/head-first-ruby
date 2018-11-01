3f6774dccb7c37f6e0f4cddd3ef5e7492527679a
cgibanez <cgibanez@tuenti.com>
2018-11-01 18:26:06 +0100

    Handle floats: use decimal points in any operand of an operation

--- charge_more.rb
+++ charge_more.rb
@@ -19,7 +19,7 @@ class Employee
 
     def print_pay_stub
         puts "Name: #{@name}"
-        pay_for_period = (@salary / 365) * 14
+        pay_for_period = (@salary / 365.0) * 14
         puts "Pay This Period: $#{pay_for_period}"
     end
 
