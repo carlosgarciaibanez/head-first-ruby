b928ad2d20f4a3938a530391ffa3c1f38b08a479
cgibanez <cgibanez@tuenti.com>
2018-11-02 11:49:26 +0100

    New HourlyEmployee and SalariedEmployee classes

--- charge_more.rb
+++ charge_more.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 
 class Employee
-    attr_reader :name, :salary
+    attr_reader :name
 
     def name=(name)
         if name == ""
@@ -9,6 +9,19 @@ class Employee
         end
         @name = name
     end
+
+    def print_name
+        puts "Name: #{name}"
+    end
+
+    def initialize(name = "Anonymous")
+        self.name = name
+    end
+
+end
+
+class SalariedEmployee < Employee
+    attr_reader :salary
 
     def salary=(salary)
         if salary < 0
@@ -18,18 +31,51 @@ class Employee
     end
 
     def initialize(name = "Anonymous", salary = 0.0)
-        self.name = name
+        super(name)
         self.salary = salary
     end
 
     def print_pay_stub
-        puts "Name: #{name}"
+        print_name
         pay_for_period = (salary / 365.0) * 14
         puts format("Pay This Period: $%.2f", pay_for_period)
+    end
+end
+
+class HourlyEmployee < Employee
+    attr_reader :hourly_wage, :hours_per_week
+
+    def hourly_wage=(hourly_wage)
+        if hourly_wage < 0
+            raise "A hourly wage of #{hourly_wage} is not valid"
+        end
+        @hourly_wage = hourly_wage
+    end
+
+    def hours_per_week=(hours_per_week)
+        if hours_per_week < 0 or hours_per_week > 24 * 7
+            raise "#{hours_per_week} is not a valid amount of hours per week"
+        end
+        @hours_per_week = hours_per_week
+    end
+
+    def initialize(name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0.0)
+        super(name)
+        self.hourly_wage = hourly_wage
+        self.hours_per_week = hours_per_week
+    end
+
+    def print_pay_stub
+        print_name
+        pay_for_period = hourly_wage * hours_per_week * 2
+        formatted_pay = format("$%.2f", pay_for_period)
+        puts "Pay This Period: #{formatted_pay}"
     end
 
 end
 
-amy = Employee.new("Amy Blake", 50000.0)
+amy = SalariedEmployee.new("Amy Blake", 50000.0)
 amy.print_pay_stub
 
+bob = HourlyEmployee.new("Bob Jameson", 7.0, 40)
+bob.print_pay_stub
