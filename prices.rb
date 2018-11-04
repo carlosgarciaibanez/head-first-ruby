e6dd0497911947388915d980ee8261acd7b2a70b
cgibanez <cgibanez@tuenti.com>
2018-11-04 19:50:21 +0100

    Use 'each' method of arrays to iterate over the elements in a collection

--- prices.rb
+++ prices.rb
@@ -1,32 +1,24 @@
-def iterate_prices(prices)
-    index = 0
-    amount = 0
-    while index < prices.length
-        amount += yield(prices[index])
-        index += 1
-    end
-    amount
-end
-
 def total(prices)
-    iterate_prices(prices) { |price| price }
+    amount = 0
+    prices.each { |price| amount += price }
+    amount
 end
 
 prices = [3.99, 25.00, 8.99]
 puts format("%.2f", total(prices))
 
 def refund(prices)
-    iterate_prices(prices) { |price| -price }
+    amount = 0
+    prices.each { |price| amount -= price }
+    amount
 end
 
 puts format("%.2f", refund(prices))
 
 def show_discounts(prices)
-    index = 0
-    while index < prices.length
-        amount_off = prices[index] / 3.0
+    prices.each do |price| 
+        amount_off = price / 3.0
         puts format("Your discount: %.2f", amount_off)
-        index += 1
     end
 end
 
