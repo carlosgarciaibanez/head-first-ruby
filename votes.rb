91ec478ee5bb6050853051fa83822d1e16e06bf3
cgibanez <cgibanez@tuenti.com>
2018-11-05 17:49:17 +0100

    Votes processing using hashes

---
+++ votes.rb
@@ -0,0 +1,15 @@
+lines = []
+File.open("votes.txt") do |votes_file|
+    lines = votes_file.readlines
+end
+
+votes = {}
+lines.each do |line|
+    name = line.chomp
+    if votes.include?(name)
+        votes[name] += 1
+    else
+        votes[name] = 1
+    end
+end
+puts votes
