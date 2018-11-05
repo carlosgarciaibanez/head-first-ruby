ea16e5b812eb3af75bc73d1eddbfd4c0ead8542c
cgibanez <cgibanez@tuenti.com>
2018-11-05 16:12:25 +0100

    Cleanup of the Reviews assignment

--- analyze_reviews.rb
+++ analyze_reviews.rb
@@ -1,19 +1,22 @@
-lines = []
-File.open("reviews.txt") do |reviews_file|
-    lines = reviews_file.readlines
-end
-
-relevant_lines = lines
-    .find_all { |line| line.include?("Truncate") }
-    .reject { |line| line.include?("\t--") }
-
 def find_adjective(string)
     words = string.split
     index = words.find_index("is")
     words[index + 1]
 end
 
-adjectives = relevant_lines.map do |line| 
+lines = []
+File.open("reviews.txt") do |reviews_file|
+    lines = reviews_file.readlines
+end
+
+reviews = lines
+    .find_all { |line| line.include?("Truncate") }
+    .reject { |line| line.include?("\t--") }
+
+
+adjectives = reviews.map do |line| 
     "'#{find_adjective(line).capitalize}'"
 end
+
+puts "The critics agree, Truncated is:"
 puts adjectives
