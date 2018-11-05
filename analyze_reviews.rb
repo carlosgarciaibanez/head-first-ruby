d92c535ca02c29a09beab3ed1f17393036a5fc58
cgibanez <cgibanez@tuenti.com>
2018-11-05 16:02:11 +0100

    Extracting the adjectives from each review line using 'find_index'

--- analyze_reviews.rb
+++ analyze_reviews.rb
@@ -3,10 +3,16 @@ File.open("reviews.txt") do |reviews_file|
     lines = reviews_file.readlines
 end
 
-puts lines.length
-
 relevant_lines = lines
     .find_all { |line| line.include?("Truncate") }
     .reject { |line| line.include?("\t--") }
 
-puts relevant_lines
+def find_adjective(string)
+    words = string.split
+    index = words.find_index("is")
+    words[index + 1]
+end
+
+adjectives = []
+relevant_lines.each { |line| adjectives << find_adjective(line) }
+puts adjectives
