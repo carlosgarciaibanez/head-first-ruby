12016fc1c17a65d35a4605d43836b85f4d850741
cgibanez <cgibanez@tuenti.com>
2018-11-05 18:51:14 +0100

    Initialize method requires many arguments

---
+++ candidate.rb
@@ -0,0 +1,21 @@
+class Candidate
+    attr_accessor :name, :age, :birthplace, :occupation, :hobby
+    def initialize(name, age, occupation, hobby, birthplace)
+        self.name = name
+        self.age = age
+        self.occupation = occupation
+        self.hobby = hobby
+        self.birthplace = birthplace
+    end
+end
+
+def print_summary(candidate)
+    puts "Candidate: #{candidate.name}"
+    puts "Age: #{candidate.age}"
+    puts "Occupation: #{candidate.occupation}"
+    puts "Hobby: #{candidate.hobby}"
+    puts "Birthplace: #{candidate.birthplace}"
+end
+
+candidate = Candidate.new("Carl Barnes", 49, "Attorney", nil, "Miami")
+print_summary(candidate)
