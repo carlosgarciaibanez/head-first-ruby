15a7cbe2dede93bd6559658a3744a53356502906
cgibanez <cgibanez@tuenti.com>
2018-11-06 10:21:51 +0100

    Use keyword parameters as a hash parameter

--- candidate.rb
+++ candidate.rb
@@ -1,11 +1,11 @@
 class Candidate
     attr_accessor :name, :age, :birthplace, :occupation, :hobby
-    def initialize(name, options = {})
+    def initialize(name, age: nil, occupation: nil, hobby: nil, birthplace: nil)
         self.name = name
-        self.age = options[:age]
-        self.occupation = options[:occupation]
-        self.hobby = options[:hobby]
-        self.birthplace = options[:birthplace]
+        self.age = age
+        self.occupation = occupation
+        self.hobby = hobby
+        self.birthplace = birthplace
     end
 end
 
