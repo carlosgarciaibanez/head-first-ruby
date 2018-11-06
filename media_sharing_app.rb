49f4daac522b57b558a085c39bbefd7358e326d3
cgibanez <cgibanez@tuenti.com>
2018-11-06 23:37:43 +0100

    New Photo class mixes-in the AcceptsComments module

--- media_sharing_app.rb
+++ media_sharing_app.rb
@@ -30,6 +30,14 @@ class Song < Clip
     attr_accessor :beats_per_minute
 end
 
+class Photo
+    include AcceptsComments
+
+    def show
+        puts "Displaying #{object_id}..."
+    end
+end
+
 video = Video.new
 video.add_comment("Cool slow motion effect!")
 video.add_comment("Weird ending.")
@@ -37,4 +45,7 @@ video.add_comment("Weird ending.")
 song = Song.new
 song.add_comment("Awesome beat.")
 
-p video.comments, song.comments
+photo = Photo.new
+photo.add_comment("Beautiful colors.")
+
+p video.comments, song.comments, photo.comments
