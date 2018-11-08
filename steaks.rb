098bfdefcb39d872c070d2ca63291503c63a7adf
cgibanez <cgibanez@tuenti.com>
2018-11-08 10:05:25 +0100

    Mixing-in the Comparable module by implementing the 'spaceship' operator

--- steaks.rb
+++ steaks.rb
@@ -1,11 +1,18 @@
 class Steak
 
+    include Comparable
+
     attr_accessor :grade
 
     GRADE_SCORES = { "Prime" => 3, "Choice" => 2, "Select" => 1 }
 
-    def > (anotherSteak)        
-        GRADE_SCORES[grade] > GRADE_SCORES[anotherSteak.grade]
+    def <=> (anotherSteak)
+        comparison = GRADE_SCORES[grade] - GRADE_SCORES[anotherSteak.grade]        
+        if comparison == 0
+            0
+        else
+            comparison / comparison.abs
+        end
     end
 end
 
@@ -17,3 +24,9 @@ second_steak.grade = "Choice"
 if first_steak > second_steak
     puts "I'll take #{first_steak.inspect}"
 end
+
+third_steak = Steak.new
+third_steak.grade = "Select"
+
+steaks = [second_steak, third_steak, first_steak]
+steaks.sort.each { |steak| p steak }
