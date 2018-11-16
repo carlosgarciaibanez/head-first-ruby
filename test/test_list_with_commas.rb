c3c067f729fb9f2b5c3866b897a8364664b0ef74
cgibanez <cgibanez@tuenti.com>
2018-11-16 19:52:23 +0100

    Adding corner cases to our tests and setting a message for the failure report

--- test/test_list_with_commas.rb
+++ test/test_list_with_commas.rb
@@ -15,4 +15,10 @@ class TestListWithCommas < MiniTest::Test
         assert('apple, orange, and pear' == list.join)
     end
 
+    def test_it_prints_one_word_alone
+        list = ListWithCommas.new
+        list.items = ['apple']
+        assert('apple' == list.join, "Return value didn't equal 'apple'")
+    end
+
 end
