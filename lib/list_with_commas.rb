16caf6b584464c20fa5acf4fbca57d18310f3e91
cgibanez <cgibanez@tuenti.com>
2018-11-16 19:42:51 +0100

    Red-Green-Refactor: unit tests to detect bugs

---
+++ lib/list_with_commas.rb
@@ -0,0 +1,13 @@
+class ListWithCommas
+    attr_accessor :items
+
+    def join
+        if items.length == 2
+            return "#{items.first} and #{items.last}"
+        end
+        last_item = "and #{items.last}"
+        other_items = items.slice(0, items.length - 1).join(", ")
+        "#{other_items}, #{last_item}"
+    end
+
+end
