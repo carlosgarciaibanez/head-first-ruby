ac1e95f90412a0acc2d642c4276d8da9dac5d52b
cgibanez <cgibanez@tuenti.com>
2018-11-16 19:54:34 +0100

    Use 'assert_equal' to check for equality

--- test/test_list_with_commas.rb
+++ test/test_list_with_commas.rb
@@ -6,19 +6,19 @@ class TestListWithCommas < MiniTest::Test
     def test_it_joins_two_words_with_and
         list = ListWithCommas.new
         list.items = ['apple', 'orange']
-        assert('apple and orange' == list.join)
+        assert_equal('apple and orange', list.join)
     end
 
     def test_it_joins_three_words_with_commas
         list = ListWithCommas.new
         list.items = ['apple', 'orange', 'pear']
-        assert('apple, orange, and pear' == list.join)
+        assert_equal('apple, orange, and pear', list.join)
     end
 
     def test_it_prints_one_word_alone
         list = ListWithCommas.new
         list.items = ['apple']
-        assert('apple' == list.join, "Return value didn't equal 'apple'")
+        assert_equal('apple', list.join)
     end
 
 end
