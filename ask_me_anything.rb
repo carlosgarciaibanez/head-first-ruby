020d485392e380f718560896c9a1b66bb9d0de72
cgibanez <cgibanez@tuenti.com>
2018-11-19 18:59:58 +0100

    Capture also the parameters passed to undefined methods

--- ask_me_anything.rb
+++ ask_me_anything.rb
@@ -1,9 +1,8 @@
 class AskMeAnything
-    def method_missing(method_name)
-        "You called #{method_name.to_s}"
+    def method_missing(method_name, arg1, arg2)
+        "You called #{method_name.to_s} with #{arg1} and #{arg2}"
     end
 end
 
 object = AskMeAnything.new
-p object.this_method_is_not_defined
-p object.also_undefined
+p object.with_args(127.0, "Hello")
